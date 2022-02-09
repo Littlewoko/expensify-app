a2f6da48441d6f2835fd5aa046603b40134258b3
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-09 18:38:28 +0000

    components for routes split

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <p>
+            <NavLink to="/" activeClassName='is-active' exact={true} >Home  </NavLink>  
+            <NavLink to="/create" activeClassName='is-active'>Create  </NavLink>  
+            <NavLink to="/edit" activeClassName='is-active'>Edit  </NavLink>  
+            <NavLink to="/help" activeClassName='is-active'>Help  </NavLink>  
+        </p> 
+    </header>
+);
+
+export default Header;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,58 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from db component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from add expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from edit component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from help component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <p>
-            <NavLink to="/" activeClassName='is-active' exact={true} >Home  </NavLink>  
-            <NavLink to="/create" activeClassName='is-active'>Create  </NavLink>  
-            <NavLink to="/edit" activeClassName='is-active'>Edit  </NavLink>  
-            <NavLink to="/help" activeClassName='is-active'>Help  </NavLink>  
-        </p> 
-    </header>
-);
+// Component Imports
+import Header from '../components/Header';
+import Dashboard from '../components/Dashboard';
+import Create from '../components/Create';
+import Edit from '../components/Edit';
+import Help from '../components/Help';
+import NotFound from '../components/NotFound';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} /> 
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
+                <Route path="/" component={Dashboard} exact={true} /> 
+                <Route path="/create" component={Create} />
+                <Route path="/edit" component={Edit} />
+                <Route path="/help" component={Help} />
+            <Route component={NotFound} />
         </Switch>  
         </div>
           
