a9a3ed1f1897f495b4c95a08378f66b8bbef5334
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-13 19:53:16 +0000

    remove from edit page

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { editExpense } from '../actions/expenses';
 import ExpenseForm from './ExpenseForm'
+import { removeExpense } from '../actions/expenses'
 
 const EditExpensePage = (props) => {
     console.log(props)
@@ -14,6 +15,10 @@ const EditExpensePage = (props) => {
                     props.history.push('/')
                 }}
             />
+            <button onClick={() => {
+                props.dispatch(removeExpense({ id: props.expense.id }))
+                props.history.push('/')
+            }}>Remove</button>
         </div>
     )
     
