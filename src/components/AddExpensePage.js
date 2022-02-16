d837123a12e6b80f5e650486a30639a3f8837838
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-16 17:45:42 +0000

    Update AddExpensePage.js

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,17 +3,20 @@ import ExpenseForm from './ExpenseForm'
 import { connect } from 'react-redux'
 import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm 
-            onSubmit={(expense) => {
-               props.onSubmit(expense)
-               props.history.push('/')
-            }}
-        />
-    </div>
-);
+class AddExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.onSubmit(expense)
+        this.props.history.push('/')
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Expense</h1>
+                <ExpenseForm onSubmit={this.onSubmit} />
+            </div>
+        )
+    }
+}
 
 const mapDispatchToProps = (dispatch) => {
     return {
