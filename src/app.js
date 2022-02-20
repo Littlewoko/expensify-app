451b8f8f5b0bcefcf450183d65d04bff54ed19d5
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-20 17:10:17 +0000

    webpack production changes

--- src/app.js
+++ src/app.js
@@ -15,13 +15,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
-store.dispatch(addExpense({ description: 'Water Bill', amount: 500 }))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 1000, createdAt: 10000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 10 }))
-
-const state = store.getState()
-console.log(getVisibleExpenses(state.expenses, state.filters))
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
