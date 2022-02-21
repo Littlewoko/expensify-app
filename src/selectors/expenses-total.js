0e7b85634283f3844a3484dc8bae7f56d521c4fb
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-21 16:41:53 +0000

    use of map and reduce

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,10 +1,14 @@
-const getExpensesTotal = (expenses) => {
+const getExpensesTotal = (expenses = []) => {
     let total = 0;
     
-    for (let i=0; i<expenses.length; i++) {
-        total = total + expenses[i].amount
+    // for (let i=0; i<expenses.length; i++) {
+    //     total = total + expenses[i].amount
+    // }
+    if (expenses !== []) {
+        const amounts = expenses.map((expense) => expense.amount) 
+        total = amounts.reduce((prev, cur) => prev + cur, total)
     }
-
+    
     return total;
 }
 
